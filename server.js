6aeb6026ef5e5d5277970d1e1c138c126cf1e5c3
Leandro Vieira <leandrown@outlook.com>
2020-01-28 17:15:55 -0300

    Conectando ao Banco de Dados Mongo

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Inicia o App
 const app = express();
 
+// Inicia conexao com o Banco de Dados Mongo
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Primeira rota definida
 app.get('/', (request, response) => {
    response.send('Hello Rocketseat');
 });
