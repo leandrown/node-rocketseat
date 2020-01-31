29f4ab40b459ae832eafeff785eb028e5b60cd99
Leandro Vieira <leandrown@outlook.com>
2020-01-31 16:43:38 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,10 +9,28 @@ module.exports = {
       return response.json(products);
    },
 
+   async show(request, response) {
+      const product = await Product.findById(request.params.id);
+      return response.json(product);
+   },
+
    async store(request, response) {
       // Criação de produtos/cadastro
       const product = await Product.create(request.body);
 
       return response.json(product);
+   },
+
+   async update(request, response) {
+      // A opçao new faz com que a variavel retorne com os valores já atualizados
+      const product = await Product.findByIdAndUpdate(request.params.id, request.body, { new: true });
+
+      return response.json(product);
+   },
+
+   async destroy(request, response) {
+      await Product.findByIdAndRemove(request.params.id);
+
+      return response.send();
    }
 };
